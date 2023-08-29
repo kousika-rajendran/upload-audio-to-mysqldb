8628d57d63ebc148f20bab6f645598f848a9d1df
kousika-rajendran <124336136+kousika-rajendran@users.noreply.github.com>
2023-08-29 08:43:24 +0530

    Create play.php

---
+++ play.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<audio controls>                                                    //audio controls such as play, pause
+<source src="<?php echo $_GET['name'];?>" type = "audio/mpeg">      //The audio file path is added as the source, so if the user clicks play the audio starts playing.
+</source>
+</body>
+</html>
